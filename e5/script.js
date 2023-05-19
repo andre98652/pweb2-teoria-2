eec5d44074e5219a02a3dfeaf4c6bf6f401ad9f6
andre98652 <132710386+andre98652@users.noreply.github.com>
2023-05-19 15:20:40 -0500

    e5 2

--- e5/script.js
+++ e5/script.js
@@ -1,26 +1,58 @@
 const obtener = document.getElementById('obtener');
 const text = document.getElementById('inputText');
-const link=document.getElementById('link');
+const box3=document.getElementById('box3');
 
 
 
 obtener.addEventListener('click',function(){
 
-    const regex = /\/([\w-]+)\?/;
-    let url=text.value;
-    let match=url.match(regex);
-    let meetCode = match ? match[1] : null;
+    let num=parseInt(text.value);
+    for(let i=1;i<=num;i++){
+        var input = document.createElement("input");
+        
+    }
+
+
     
-
-    meetCode=meetCode.split("-").join(" ");
-    obtener.classList.add('hidden');
-    text.classList.add('hidden');
-
-    link.style.fontSize="24px";
-    link.textContent=meetCode;
 });
 
+/*
+    function crearInputs() {
+      var box2 = document.getElementById("box2");
+      var numero = parseInt(box2.value);
 
+      var box3 = document.getElementById("box3");
+      box3.innerHTML = "";
+
+      for (var i = 1; i <= numero; i++) {
+        var input = document.createElement("input");
+        input.type = "number";
+        input.name = "input" + i;
+        box3.appendChild(input);
+        box3.appendChild(document.createElement("br"));
+      }
+
+      var sumarButton = document.createElement("button");
+      sumarButton.innerHTML = "Sumar";
+      sumarButton.onclick = sumarInputs;
+      box3.appendChild(sumarButton);
+    }
+
+    function sumarInputs() {
+      var box3 = document.getElementById("box3");
+      var inputs = box3.getElementsByTagName("input");
+      var suma = 0;
+
+      for (var i = 0; i < inputs.length; i++) {
+        var valor = parseInt(inputs[i].value);
+        if (!isNaN(valor)) {
+          suma += valor;
+        }
+      }
+
+      alert("La suma es: " + suma);
+    }
+*/
     
 
     
