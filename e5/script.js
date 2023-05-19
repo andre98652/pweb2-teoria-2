6fa330b6cb6b84071b36656fc43b7df8762d8901
andre98652 <132710386+andre98652@users.noreply.github.com>
2023-05-19 14:57:48 -0500

    e5 ini

---
+++ e5/script.js
@@ -0,0 +1,26 @@
+const obtener = document.getElementById('obtener');
+const text = document.getElementById('inputText');
+const link=document.getElementById('link');
+
+
+
+obtener.addEventListener('click',function(){
+
+    const regex = /\/([\w-]+)\?/;
+    let url=text.value;
+    let match=url.match(regex);
+    let meetCode = match ? match[1] : null;
+    
+
+    meetCode=meetCode.split("-").join(" ");
+    obtener.classList.add('hidden');
+    text.classList.add('hidden');
+
+    link.style.fontSize="24px";
+    link.textContent=meetCode;
+});
+
+
+    
+
+    
