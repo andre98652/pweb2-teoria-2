c5cd9ca182151d8317667249b6dc63d58817510b
andre98652 <132710386+andre98652@users.noreply.github.com>
2023-05-22 18:15:35 -0500

    Update script.js

--- e5/script.js
+++ e5/script.js
@@ -1,89 +1,44 @@
-const obtener = document.getElementById('obtener');
-const text = document.getElementById('inputText');
-const box3=document.getElementById('box3');
-const box2=document.getElementById('box2');
+document.getElementById("sumForm").addEventListener("submit", function(event) {
+  event.preventDefault(); // Evita que el formulario se envíe
 
+  var numForms = parseInt(document.getElementById("numForms").value);
+  if (isNaN(numForms) || numForms < 1) {
+    return; // Verifica si el número ingresado es válido
+  }
 
-obtener.addEventListener('click',function(){
+  var formsContainer = document.getElementById("formsContainer");
+  formsContainer.innerHTML = ""; // Limpia los formularios existentes
 
-    let num=parseInt(text.value);
-    for(let i=1;i<=num;i++){
-        var input = document.createElement("input");
-        input.type = "number";
-        input.name = "input" + i;
+  for (var i = 0; i < numForms; i++) {
+    var formGroup = document.createElement("div");
+    formGroup.className = "formGroup";
 
-        text.classList.add("hidden");
-        obtener.classList.add("hidden");
-        box3.appendChild(input);
-        box3.appendChild(document.createElement("br"));
+    var label = document.createElement("label");
+    label.textContent = "Número " + (i + 1) + ":";
+    formGroup.appendChild(label);
 
-        
+    var input = document.createElement("input");
+    input.type = "number";
+    input.required = true;
+    formGroup.appendChild(input);
 
+    formsContainer.appendChild(formGroup);
+  }
 
-
-    }
-    var boton = document.createElement("button");
-
-    boton.innerHTML="Sumar";
-    boton.onclick=sumar;
-    box3.appendChild(boton);
-
-
-    function sumar(){
-      var box3=document.getElementById("box3");
-      var inputs=box3.getElementsByTagName("input");
-      let suma=0;
-
-      for(let i=0;i<inputs.length;i++){
-        let valor=parseInt(inputs[i].value);
-
-
-        if(!isNaN(valor)){
-          suma+=valor;
-        }
-      }
-      console.log(suma);
-    };
+  var sumButton = document.getElementById("sumButton");
+  sumButton.disabled = false; // Habilita el botón de sumar
 });
 
+document.getElementById("sumButton").addEventListener("click", function() {
+  var inputs = document.querySelectorAll("#formsContainer input");
+  var sum = 0;
 
-/*
-    function crearInputs() {
-      var box2 = document.getElementById("box2");
-      var numero = parseInt(box2.value);
-
-      var box3 = document.getElementById("box3");
-      box3.innerHTML = "";
-
-      for (var i = 1; i <= numero; i++) {
-        var input = document.createElement("input");
-        input.type = "number";
-        input.name = "input" + i;
-        box3.appendChild(input);
-        box3.appendChild(document.createElement("br"));
-      }
-
-      var sumarButton = document.createElement("button");
-      sumarButton.innerHTML = "Sumar";
-      sumarButton.onclick = sumarInputs;
-      box3.appendChild(sumarButton);
+  for (var i = 0; i < inputs.length; i++) {
+    var value = parseInt(inputs[i].value);
+    if (!isNaN(value)) {
+      sum += value;
     }
+  }
 
-    function sumarInputs() {
-      var box3 = document.getElementById("box3");
-      var inputs = box3.getElementsByTagName("input");
-      var suma = 0;
-
-      for (var i = 0; i < inputs.length; i++) {
-        var valor = parseInt(inputs[i].value);
-        if (!isNaN(valor)) {
-          suma += valor;
-        }
-      }
-
-      alert("La suma es: " + suma);
-    }
-*/
-    
-
-    
+  document.getElementById("result").innerHTML = "La suma es: " + sum;
+});
